df4aba9edff2c44d5063d91f817e5e67906f284d
Fergus <fergus.jingtai@gmail.com>
2017-12-11 14:25:46 +0800

    7. Passing Dynamic data with props

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react'
 class Header extends Component {
 	render() {
 		return (
-			<div>
-				Header
-			</div>
+			<header className="top">
+				<h1>
+          Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day
+        </h1>
+				<h3 className="tagline"><span>{this.props.tagline}</span></h3>
+			</header>
 		)
 	}
 }
